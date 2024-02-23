2d2ac8016969fff74d57e3ddc74d909154c352be
Shivam Gupta <shivam@krishiyog.in>
2024-02-23 21:52:52 +0530

    Made some changes.

--- app/src/main/java/com/shivamgupta/newsapp/util/Constants.kt
+++ app/src/main/java/com/shivamgupta/newsapp/util/Constants.kt
@@ -1,9 +1,7 @@
 package com.shivamgupta.newsapp.util
 
 object Constants {
-    //const val API_KEY = "2623a1b205ca4ec581e11927fb268171"
-    //const val API_KEY = "1547aedce7d848f587f47a1748b3b868"
-    const val API_KEY = "1d6d3337110c4981892c05cb61503f90"
+    const val API_KEY = "aea54daf365c44e4b2c953744524be2c"
     const val BASE_URL = "https://newsapi.org"
     const val VISIBLE_TEXT_LENGTH = 18
     const val API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
